9491a37917e4fccd220b132d658844bf74c602d6
Luis Sánchez-Arévalo Rionegro <luis.sanchezarevalo.developer@gmail.com>
2020-06-22 21:33:10 +0000

    Resolved exercises

--- exercises/04-conditional-rendering/index.js
+++ exercises/04-conditional-rendering/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Alert = props => {
-	//add the condition inside this function
-
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+	if (props.show) {
+		return (
+			<div className="alert alert-danger" role="alert">
+				{props.text}
+			</div>
+		);
+	} else {
+		return null;
+	}
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
+	show: PropTypes.bool
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
